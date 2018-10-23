d55b8d7b96a0c0501410ec4c32b8556ef3fb9972
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2018-10-23 17:27:58 +0200

    Add old browsers compatibility

--- addon/services/visibility.js
+++ addon/services/visibility.js
@@ -1,25 +1,89 @@
 import Service from '@ember/service';
 import { bind } from '@ember/runloop';
+import { get } from '@ember/object';
+import { isPresent } from '@ember/utils';
+
+export const APIs = {
+  global: { isSupported: true, hiddenFlag: 'hidden', eventName: 'visibilitychange' },
+  webkit: { isSupported: true, hiddenFlag: 'webkitHidden', eventName: 'webkitvisibilitychange' },
+  mozilla: { isSupported: true, hiddenFlag: 'mozHidden', eventName: 'mozvisibilitychange' },
+  ie: { isSupported: true, hiddenFlag: 'msHidden', eventName: 'msvisibilitychange' },
+  unsupported: { isSupported: false }
+};
+
+/**
+ * Returns visibility API support flag & flag / event names, depending on the
+ * user's browser.
+ *
+ * See also:
+ * https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
+ * https://whatwebcando.today/foreground-detection.html
+ */
+export const detectApi = function(_document = {}) {
+  if (isPresent(get(_document, 'hidden'))) { return APIs.global; }
+  if (isPresent(get(_document, 'webkitHidden'))) { return APIs.webkit; }
+  if (isPresent(get(_document, 'mozHidden'))) { return APIs.mozilla; }
+  if (isPresent(get(_document, 'msHidden'))) { return APIs.ie; }
+
+  return APIs.unsupported;
+};
 
 /**
  * This service keeps track of whether the window has ever lost visibility. This
  * happens when the user switches tabs, minimizes the window, etc.
  */
 export default Service.extend({
+
   visible: true,
+
   lostVisibility: false,
+
+  pageVisibilityAPI: detectApi(document),
 
   init() {
     this._super(...arguments);
-    this._handleDocumentVisibilityChange();
 
-    document.addEventListener('visibilitychange', bind(this, '_handleDocumentVisibilityChange'));
+    const { isSupported, eventName } = this.get('pageVisibilityAPI');
+
+    if (isSupported) {
+      this._handleDocumentVisibilityChange();
+
+      document.addEventListener(eventName, bind(this, '_handleDocumentVisibilityChange'));
+    }
   },
 
+  /**
+   * Service teardown
+   */
+  willDestroy() {
+    this._super(...arguments);
+
+    this.removeBindings();
+  },
+
+  /**
+   * Unsubscribes to events.
+   */
+  removeBindings() {
+    const { isSupported, eventName } = this.get('pageVisibilityAPI');
+
+    if (isSupported) {
+      document.removeEventListener(eventName, this.get('_handleDocumentVisibilityChange'));
+    }
+  },
+
+  /**
+   * Event handler.
+   */
   _handleDocumentVisibilityChange() {
+    const hiddenFlagName = this.get('pageVisibilityAPI.hiddenFlag');
+    const isHidden = get(document, hiddenFlagName);
+
     if (this.isDestroyed) { return; }
-    this.set('visible', !document.hidden);
-    if (document.hidden) {
+
+    this.set('visible', !isHidden);
+
+    if (isHidden) {
       this.set('lostVisibility', true);
     }
   }
